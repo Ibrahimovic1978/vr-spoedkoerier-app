694300f39d7fffcc4f0603848d4721e3f08cc1be
Ibrahimovic1978 <vanrozenbv@gmail.com>
2026-08-17 11:08:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.VR_CONFIG = {
   SUPABASE_URL: "https://xogybtmkjdalbfkjyoxd.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_40_EGUy6TAfjYdOX_hyUiQ_y1r3KRZu",
-  GOOGLE_MAPS_API_KEY: ""
+  ORS_API_KEY: "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImMyM2U2MGQxYjgxZTQ2NzdhMTQyM2M0ZjE3NTVkYmQyIiwiaCI6Im11cm11cjY0In0="
 };
